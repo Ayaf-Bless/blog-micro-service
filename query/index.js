1114f2b602c15d5dd9518836c4e69b75c07af324
ayaf-bless <blessambel1@gmail.com>
2021-12-12 17:24:10 +0300

    fix latency with query service

--- query/index.js
+++ query/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const { randomBytes } = require("crypto");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(express.json());
@@ -8,11 +8,7 @@ app.use(cors());
 
 const posts = {};
 
-app.get("/posts", (req, res) => {
-  res.send(posts);
-});
-app.post("/events", (req, res) => {
-  const { type, data } = req.body;
+const handleEvent = (type, data) => {
   if (type === "postCreated") {
     const { id, title } = data;
     posts[id] = { id, title, comments: [] };
@@ -31,9 +27,25 @@ app.post("/events", (req, res) => {
     comment.status = status;
     comment.content = content;
   }
+};
+
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
+
+app.post("/events", (req, res) => {
+  const { type, data } = req.body;
+  handleEvent(type, data);
+
   res.send({});
 });
 
-app.listen(4002, () => {
+app.listen(4002, async () => {
   console.log("app is listening on 4002/ Query");
+
+  const res = await axios.get("http://localhost:4005/events");
+  for (let event of res.data) {
+    console.log("Processing events", event.type);
+    handleEvent(event.type, event.data);
+  }
 });
